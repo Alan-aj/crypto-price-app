1991000e604040061c721ad14ab3d83697f2aa8f
Alan-aj <alanj14333@gmail.com>
2022-01-24 10:54:53 +0530

    final commit

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -4,8 +4,8 @@ function Coin({ name, icon, price, symbol }) {
   return ( 
     <div className='coin'>
         <h1>Name: {name} </h1>
-        <img src={icon} />
-        <h3>Price: {price} </h3>
+        <img src={icon} alt={name}/>
+        <h3>Price: {price.toFixed(3)}$ </h3>
         <h3>Symbol: {symbol} </h3>
     </div>
   )
